12c90e30ea6f8be86e16eb7bdce72b5839056b88
wilderif <woojuro3@gmail.com>
2024-06-22 23:10:08 +0900

    feat(Behind_the_Scenes): - Used useCallback to prevent recreation of functions inside components and avoid re-rendering of child components that use these functions as props - Applied useMemo to prevent unnecessary re-execution of non-component functions

--- Behind_the_Scenes/src/components/Counter/Counter.jsx
+++ Behind_the_Scenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, memo, useCallback, useMemo } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -25,17 +25,20 @@ function isPrime(number) {
 
 const Counter = memo(function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
