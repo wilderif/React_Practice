e86f9c4f61a54dfbe34d7232664e8b04b9637f16
wilderif <woojuro3@gmail.com>
2024-06-26 17:29:59 +0900

    feat(Place_Picker_2): add loading and error handling to AvailablePlaces
    - Display loadingText while fetching places data.
    - Implement error handling for fetching process.
    - Show Error component on fetch error.

--- Place_Picker_2/src/components/AvailablePlaces.jsx
+++ Place_Picker_2/src/components/AvailablePlaces.jsx
@@ -1,24 +1,50 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchPlaces = async () => {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          const error = new Error("Failed to fetch places");
+          throw error;
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
+      setIsFetching(false);
     };
 
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
