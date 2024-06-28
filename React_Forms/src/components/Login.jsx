40504d1077df7d14ece954c4d88cea04ee526db0
wilderif <woojuro3@gmail.com>
2024-06-28 14:37:07 +0900

    feat(React_Forms): Add form submission prevention and state management for email and password
    - Implemented onSubmit property on form and preventDefault to prevent server submission and page reload on form submission.
    - Added enteredValues state to manage input values for email and password.
    - Created handleInputChange function to update state based on user input.

--- React_Forms/src/components/Login.jsx
+++ React_Forms/src/components/Login.jsx
@@ -1,23 +1,59 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleInputChange = (identifier, value) => {
+    setEnteredValues((prevValues) => {
+      return {
+        ...prevValues,
+        [identifier]: value,
+      };
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="button" className="button" onClick={handleSubmit}>
+          Login
+        </button>
       </p>
     </form>
   );
