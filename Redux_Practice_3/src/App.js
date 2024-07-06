d276550abff828d856025a1d038d0bac47509c52
wilderif <woojuro3@gmail.com>
2024-07-06 18:29:27 +0900

    feat(Redux_Practice_3): Integrate Firebase Realtime Database with Redux store
    - Added useEffect to App component to send cart data to Firebase Realtime Database.
    - Configured useSelector to monitor changes in the cart state and update Firebase accordingly.
    - Utilized Firebase's PUT method to update cart data whenever the Redux store's cart state changes.

--- Redux_Practice_3/src/App.js
+++ Redux_Practice_3/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart.js";
@@ -6,6 +7,14 @@ import Products from "./components/Shop/Products.js";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-practice-f9a22-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
