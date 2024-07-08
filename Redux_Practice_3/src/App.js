db5fafbcb2878cb900a1af6cc50c91cc89fbcbe5
wilderif <woojuro3@gmail.com>
2024-07-09 03:45:04 +0900

    feat(Redux_Practice_3): Implement async action creator to send cart data to Firebase
    - Implemented sendCartData async action to send cart data to Firebase Realtime Database.
    - Added pending, success, and error notifications during the cart data sending process.
    - Configured notifications to show the status of cart data operations.

--- Redux_Practice_3/src/App.js
+++ Redux_Practice_3/src/App.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { uiActions } from "./store/ui-slice.js";
 import Cart from "./components/Cart/Cart.js";
 import Layout from "./components/Layout/Layout.js";
 import Products from "./components/Shop/Products.js";
 import Notification from "./components/UI/Notification.js";
+import { sendCartData } from "./store/cart-slice.js";
 
 let isInitial = true;
 
@@ -16,46 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://redux-practice-f9a22-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
