4beabc022b4f703df18a3043a856621eeff98059
wilderif <woojuro3@gmail.com>
2024-07-11 17:42:04 +0900

    feat(React_Router_2): Update HomePage component with PageContent component

--- React_Router_2/frontend/src/pages/Home.jsx
+++ React_Router_2/frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import PageContent from "../components/PageContent.jsx";
+
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <PageContent title="Welcome!">
+        <p>Browse all our amazing events!</p>
+      </PageContent>
     </>
   );
 };
