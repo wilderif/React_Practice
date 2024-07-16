a34d94fd5145aff38f162e5a5aa242fa5a9de9be
wilderif <woojuro3@gmail.com>
2024-07-17 01:53:58 +0900

    feat(React_Query): Update NewEvent to trigger refetch events
    - Moved queryClient definition from App component to util.js to allow usage across multiple files.
    - Added onSuccess property to useMutation in NewEvent component.
    - Ensured that upon successful mutation, queryClient.invalidateQueries({ queryKey: ['events'] }) is called to refetch events including the new one.
    - Implemented navigation to '/events' route after a successful mutation.

--- React_Query/src/App.jsx
+++ React_Query/src/App.jsx
@@ -3,8 +3,9 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
+import { queryClient } from "./util/http.js";
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
@@ -37,8 +38,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
--- React_Query/src/components/Events/NewEvent.jsx
+++ React_Query/src/components/Events/NewEvent.jsx
@@ -1,16 +1,21 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 
+import { queryClient } from "../../util/http.js";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { createNewEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { createNewEvent } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
   });
 
   function handleSubmit(formData) {
